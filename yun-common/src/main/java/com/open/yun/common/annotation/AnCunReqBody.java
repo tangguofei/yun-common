131374ac87c3ea49ea771469b685efab4ed80b69
tangguofei@ancun.com <tangguofei@ancun.com>
2017-03-29 16:10:44 +0800

    remove comment

--- yun-common/src/main/java/com/open/yun/common/annotation/AnCunReqBody.java
+++ yun-common/src/main/java/com/open/yun/common/annotation/AnCunReqBody.java
@@ -8,14 +8,6 @@ import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.ElementType.TYPE;
 
-/**
- * 保全中心请求体{"request"：{"content" : {} }}格式指定
- *
- * @Created on 2015年10月15日
- * @author 摇光
- * @version 1.0
- * @Copyright:杭州安存网络科技有限公司 Copyright (c) 2015
- */
 @Target({ TYPE, METHOD, PARAMETER })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface AnCunReqBody {
